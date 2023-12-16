1f64620aa3d07531393152f26e2a0bbeb1837164
jason-napolitano <jnapolitanoit@gmail.com>
2023-12-15 20:52:47 -0800

    Update helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,17 @@
 <?php
+
+use Illuminate\Contracts\Auth\Authenticatable;
+
+// If the function doesn't exist, let's create it!
+if ( ! function_exists('user')) {
+    /**
+     * Returns the current user. Shorthand for
+     * auth()->user()
+     *
+     * @return Authenticatable|null
+     */
+    function user(): ?Authenticatable
+    {
+        return auth()->user();
+    }
+}
